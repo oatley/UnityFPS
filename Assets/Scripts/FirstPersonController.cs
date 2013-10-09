b04df901ff5f2144bef324143bdfd334bb815f61
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2013-10-09 16:01:06 -0400

    First commit
    First person game using unity

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+	
+	public float movementSpeed = 5.0f;
+	public float mouseSensitivity = 2.0f;
+	public float upDownRange = 60.0f;
+	float verticalRotation = 0; 
+	
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//Rotation
+		float rotLeftRight = Input.GetAxis("Mouse X") * mouseSensitivity;
+		
+		transform.Rotate(0, rotLeftRight, 0);
+		
+		//Can change this to use camera function later
+		verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+		verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+		Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0,0);
+		
+		
+		
+		//Movement
+		float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
+		float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+		
+		Vector3 speed = new Vector3(sideSpeed, 0, forwardSpeed);
+		
+		speed = transform.rotation * speed;
+		
+		CharacterController cc = GetComponent<CharacterController>();
+	
+		cc.SimpleMove(speed);
+	}
+}
